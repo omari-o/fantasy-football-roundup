995329b57cc0b343aa51ff7be4f320aa68090802
Omari Lewis <omarilewis97@gmail.com>
2025-09-09 16:18:44 -0400

    Add Biggest Benchwarmer stat

--- report.py
+++ report.py
@@ -27,10 +27,26 @@ print("\nClosest match: %s vs %s by %.2f points" % (closest[0].home_team.team_na
                                                     closest[0].away_team.team_name,
                                                     closest[1]) )
 blowout = max(diffs, key=lambda x: x[1])
-print("Biggest blowout: %s vs %s by %.2f points" % (blowout[0].home_team.team_name,
-                                                    blowout[0].away_team.team_name,
-                                                    blowout[1]) )
+print("BTA Match of the Week: %s vs %s by %.2f points" % (blowout[0].home_team.team_name,
+                                                          blowout[0].away_team.team_name,
+                                                          blowout[1]) )
 
 # get lowest and highest scorer
 print(f"\nBest Team this Week: {league.top_scored_week()}")
-print(f"Worst Team this Week: {league.least_scored_week()}")
+print(f"Worst Team this Week: {league.least_scored_week()}\n")
+
+# left the biggest points on their bench
+benchheater = ("", teams[0].roster[0], 0)
+for t in teams:
+    bench = [(t, p.name, p.stats[1]['points']) for p in t.roster if p.lineupSlot == 'BE']
+    benchwarmer = max(bench, key=lambda x: x[2])
+    benchheater = benchwarmer if benchwarmer[2] > benchheater[2] else benchheater
+
+print("%s racked up %s points on %s's bench.. " % (benchheater[1],
+                                                   benchheater[2],
+                                                   benchheater[0].team_name),
+                                                   end='')
+if benchheater[0].outcomes[0] == 'W':
+    print("but luckily they still got the dub\n")
+else:
+    print("and they sure as hell could've used them too\n")
